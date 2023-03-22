98b32e69d4eb6febb4c3fd796c87b01ae343d12e
shree sharma <85297469+shree12-cyber@users.noreply.github.com>
2023-03-22 17:12:01 +0530

    Add files via upload

---
+++ src/components/Home/Category/Category.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import "./Category.scss";
+
+const Category = ({ categories }) => {
+  const navigate= useNavigate();
+  // if (categories !== undefined){
+  return ( 
+    <div className="shop-by-category">
+      <div className="categories">
+        {categories?.data?.map((item) => (
+          <div key={item.id} className="category" onClick={()=>navigate(`/category/${item.id}  `)} >
+            <img src={process.env.REACT_APP_DEV_URL + item?.attributes?.img.data?.[0]?.attributes?.url} alt="" />
+          </div>
+        ))}
+      </div>
+    </div>
+
+  );
+        }
+// };
+
+export default Category;
